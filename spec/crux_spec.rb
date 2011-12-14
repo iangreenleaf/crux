0f21c311559d58508663e705fd5f88f661b60389
Ian Young <ian.greenleaf@gmail.com>
2011-12-13 23:50:14 -0800

    Stub out network requests with fakeweb

--- spec/crux_spec.rb
+++ spec/crux_spec.rb
@@ -29,8 +29,17 @@ describe "crux" do
       end
     end
 
-    it "fetches @require urls" do
-      File.exists?("build/vendor/jquery.min.js").should be_true
+    context "@require urls" do
+      it "fetches resources" do
+        FakeWeb.last_request.method.should == "GET"
+        FakeWeb.last_request.path.should == "/ajax/libs/jquery/1.6.4/jquery.min.js"
+      end
+      it "creates files in the build dir" do
+        File.exists?("build/vendor/jquery.min.js").should be_true
+      end
+      it "puts content into files" do
+        IO.read("build/vendor/jquery.min.js", JS_CONTENT.bytesize + 1).should == JS_CONTENT
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,8 @@ end
 def json_file file
   JSON.load File.open(file)
 end
+
+JS_CONTENT = "/* Fake Javascript file */ alert('foo');"
+require 'fakeweb'
+FakeWeb.allow_net_connect = false
+FakeWeb.register_uri :get, %r|.*\.js|, :body => JS_CONTENT
